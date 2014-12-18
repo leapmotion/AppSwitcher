16598a2b4a0280d332513cf8a0cb191953007a4e
Jason Lokerson <cmercenary@gmail.com>
2014-12-17 17:21:38 -0800

    Initial version of the project

---
+++ AppSwitcher/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace AppSwitcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctRift = new System.Windows.Forms.PictureBox();
+            this.pctRegular = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRift)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRegular)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctRift
+            // 
+            this.pctRift.Image = global::AppSwitcher.Properties.Resources.smiley;
+            this.pctRift.Location = new System.Drawing.Point(214, 97);
+            this.pctRift.Name = "pctRift";
+            this.pctRift.Size = new System.Drawing.Size(141, 124);
+            this.pctRift.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctRift.TabIndex = 0;
+            this.pctRift.TabStop = false;
+            this.pctRift.Click += new System.EventHandler(this.OnDirectRiftExeLaunch);
+            // 
+            // pctRegular
+            // 
+            this.pctRegular.Image = global::AppSwitcher.Properties.Resources.smiley;
+            this.pctRegular.Location = new System.Drawing.Point(33, 97);
+            this.pctRegular.Name = "pctRegular";
+            this.pctRegular.Size = new System.Drawing.Size(141, 124);
+            this.pctRegular.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctRegular.TabIndex = 1;
+            this.pctRegular.TabStop = false;
+            this.pctRegular.Click += new System.EventHandler(this.OnRegularExeLaunch);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 318);
+            this.Controls.Add(this.pctRegular);
+            this.Controls.Add(this.pctRift);
+            this.Name = "MainForm";
+            this.Text = "[Game Title String]";
+            this.Load += new System.EventHandler(this.OnLoaded);
+            ((System.ComponentModel.ISupportInitialize)(this.pctRift)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRegular)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctRift;
+        private System.Windows.Forms.PictureBox pctRegular;
+    }
+}
+
---
+++ AppSwitcher/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AppSwitcher
+{
+    public partial class MainForm : Form
+    {
+        public string directToRiftExe;
+        public string regularExe;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("switcher.xml");
+
+            XmlNode root = doc.DocumentElement;
+            for (XmlNode cur = root.FirstChild; cur != null; cur = cur.NextSibling)
+            {
+                if (cur.Name.CompareTo("WindowTitle") == 0)
+                    this.Text = cur.InnerText;
+                else if (cur.Name.CompareTo("DirectRiftExe") == 0)
+                    directToRiftExe = cur.InnerText;
+                else if (cur.Name.CompareTo("RegularExe") == 0)
+                    regularExe = cur.InnerText;
+            }
+        }
+
+        private void OnRegularExeLaunch(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(regularExe);
+        }
+
+        private void OnDirectRiftExeLaunch(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(directToRiftExe);
+        }
+    }
+}
